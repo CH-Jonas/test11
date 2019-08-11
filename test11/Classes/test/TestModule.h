ec6e445c03c8b22528ecd8ec92364c6cc1ab1e51
田啟东 <353283899@qq.com>
2019-08-11 23:07:41 +0800

    1

---
+++ test11/Classes/test/TestModule.h
@@ -0,0 +1,17 @@
+//
+//  TestModule.h
+//  TestModule
+//
+//  Created by 冲 on 2019/8/6.
+//  Copyright © 2019年 CH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModule : NSObject
+-(void)run;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test11/Classes/test/TestModule.m
@@ -0,0 +1,15 @@
+//
+//  TestModule.m
+//  TestModule
+//
+//  Created by 冲 on 2019/8/6.
+//  Copyright © 2019年 CH. All rights reserved.
+//
+
+#import "TestModule.h"
+
+@implementation TestModule
+-(void)run{
+    NSLog(@"测试测试  哈哈哈哈");
+}
+@end
